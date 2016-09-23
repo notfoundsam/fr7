d42b7c63f22731257da58d224b1542c111d2a257
notfound <notfoundsam@gmail.com>
2016-09-23 22:32:58 +0900

    add rebid

--- js/my-app.js
+++ js/my-app.js
@@ -1,7 +1,7 @@
 // var ajax_host = 'http://yahooauc.dev';
-// var ajax_host = '';
+var ajax_host = '';
 var current_bidder = '';
-var ajax_host = 'http://yahooauc-servletyahoo.rhcloud.com';
+// var ajax_host = 'http://yahooauc-servletyahoo.rhcloud.com';
 
 // Initialize app
 var myApp = new Framework7({
@@ -29,8 +29,15 @@ $$.ajax({
 		var d_obj = JSON.parse(data);
 		if (d_obj.status_code == 100) {
 			$$('#username').text(d_obj.result.current_user);
-			console.log(d_obj.result.current_user);
 			current_bidder = d_obj.result.current_bidder;
+
+			mainView.router.load({
+				url: 'bid.html'
+				// reload: true,
+				// context: {
+				// 	title: 'Bid'
+				// }
+			});
 		}
 	},
 	statusCode: {
@@ -55,8 +62,6 @@ $$('#bidding').on('click', function() {
 		success: function (data) {
 			var d_obj = JSON.parse(data);
 			if (d_obj.status_code == 100) {
-				myApp.hideIndicator();
-
 				var auctions = d_obj.result.auctions ? d_obj.result.auctions : null;
 
 				mainView.router.load({
@@ -67,12 +72,12 @@ $$('#bidding').on('click', function() {
 						auctions: auctions,
 						current_bidder: current_bidder
 					}
-				})
-				console.log(d_obj.result);
+				});
+				$$('#bidding_count').text(auctions.length);
 			} else {
-				myApp.hideIndicator();
 				console.log(d_obj.message);
 			}
+			myApp.hideIndicator();
 		}
 	});
 });
@@ -99,6 +104,9 @@ $$('#login').on('click', function() {
 				case 10: 
 					$$('#username').text(d_obj.result.current_user);
 					current_bidder = d_obj.result.current_bidder;
+					mainView.router.load({
+						url: 'bid.html'
+					});
 					myApp.closeModal('.login-screen');
 					break;
 				case 20: 
@@ -126,3 +134,78 @@ $$('#logout').on('click', function() {
 		}
 	});
 });
+
+myApp.onPageInit('bid', function (page) {
+	$$('#bid').on('click', function() {
+
+		var auc_id = $$('input[name=auc_id]').val();
+		var price = $$('input[name=price]').val();
+
+		if (!auc_id || !price) {
+			myApp.alert('Enter both Lot ID and Price', 'Input error');
+			return;
+		}
+
+		myApp.showIndicator();
+
+		bid(auc_id, price, false);
+	});
+});
+
+myApp.onPageInit('bidding', function (page) {
+	$$('.bid-up').on('click', function() {
+
+		var auc_id = $$(this).attr('data-id');
+
+		myApp.prompt('Lot ID: ' + auc_id, 'Set new price', function (value) {
+			var price = parseInt(value);
+			if (!price) {
+				return;
+			}
+			
+			myApp.showIndicator();
+
+			bid(auc_id, price, true);
+		});
+	});
+});
+
+function bid(auc_id, price, rebid) {
+	$$.ajax({
+		url: ajax_host + '/admin/api/bid',
+		type: 'POST',
+		data: {
+			auc_id: auc_id,
+			price: price
+		},
+		statusCode: {
+			401: function (xhr) {
+				myApp.hideIndicator();
+				myApp.loginScreen();
+			}
+		},
+		success: function (data) {
+			var d_obj = JSON.parse(data);
+			if (d_obj.status_code == 100) {
+
+				if (rebid) {
+					var card = $$('a[data-id=' + auc_id + ']').closest('.card');
+					card.removeClass('card-red').addClass('card-green');
+				}
+
+				myApp.addNotification({
+					message: d_obj.result,
+					hold: 3000,
+					additionalClass: 'bid-success'
+				});
+			} else {
+				myApp.addNotification({
+					message: d_obj.error,
+					hold: 3000,
+					additionalClass: 'bid-error'
+				});
+			}
+			myApp.hideIndicator();
+		}
+	});
+}
